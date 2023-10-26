a01388018c1053421f662b067163073a5106f9d2
Daniel Lipkin <daniel.lipkin@tum.de>
2023-10-26 12:07:33 +0200

    using Command span as additional parameter for proof_improve

---
+++ Proof_Improve.thy
@@ -0,0 +1,17 @@
+(*  Title:      HOL/Proof_Improve.thy
+    Author:     Daniel Lipkin, TU Muenchen
+*)
+
+section ‹Proof Improve: Automatic Proof Improvement›
+
+theory Proof_Improve
+  imports Sledgehammer
+begin
+
+
+ML_file ‹Tools/Proof_Improve/proof_improve_config_manager.ML›
+ML_file ‹Tools/Proof_Improve/proof_improve_scorer.ML›
+ML_file ‹Tools/Proof_Improve/proof_improve_finder.ML›
+ML_file ‹Tools/Proof_Improve/proof_improve.ML›
+
+end
